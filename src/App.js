a923923b24d90cc56a87dbd97e58285023147688
arjunesarkar <arjunesarker.bd@gmail.com>
2023-12-29 13:14:41 +0600

    frontEnd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navber from './Components/navbar/Navber';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Excercise from './Components/Excercise/Excercise';
+import CreateExcercise from './Components/Excercise/CreateExcercise';
+import User from './Components/User/User';
+import CreateUser from './Components/User/CreateUser';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navber />
+      <br />
+      <Routes>
+        <Route path="/excercise" element={<Excercise />} />
+        <Route path="/createexcercise" element={<CreateExcercise />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/createuser" element={<CreateUser />} />
+      </Routes>
+    </Router>
   );
 }
 
